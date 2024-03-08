cc82b6c859497dccfcf265a1c563097f3bb4ac0d
Alexis Cuevas <ceroweb@gmail.com>
2024-03-07 22:01:13 -0300

    Actualizando Primer Programa

---
+++ Dia1/mi_primer_programa.py
@@ -0,0 +1,21 @@
+nombre = "Alexis"
+apellido1 = "Cuevas"
+apellido2 = "Vasquez"
+edad="46"
+#Python no permite concatenar(sumar) letras con numeros
+#suma de string = concatenar
+#string-> cadena de caracteres (se escribe entre comillas)
+año=2024 #es un numero
+
+print("****** Informacion *********")
+print("Tu nombre es " + nombre )
+print("Tu apellido paterno es " + apellido1 )
+print("Tu apellido materno es " + apellido2 )
+print("tu tienes la edad de       " + edad )
+print("el año es",año," mes ",3," dia ",7)
+print("****************************")
+
+edad=18
+direccion='parcela 6'
+edad = 21.5
+edad = "50"
